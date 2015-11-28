b15f798ef1c1e8274345ee90832ed7026de690c7
Krzysztof Nagrodkiewicz <kn334812@mimuw.edu.pl>
2015-11-28 05:00:37 -0800

    Added reading reads.

--- zadanie_1/main.py
+++ zadanie_1/main.py
@@ -1,1 +1,18 @@
-__author__ = 'kris'
+def read_reads(plik):
+	with open(plik, 'r') as f:
+		lines = [line.rstrip('\n').split() for line in f]
+	return lines
+
+
+def compute_coverage(odczyty):
+	return None
+
+
+def write_coverage(pokrycie, plik_wyjsciowy):
+	return None
+
+
+def multiply_coverage(plikA, plikB, plik_wyjsciowy):
+	return None
+
+print read_reads('odczytyA.sdx')
