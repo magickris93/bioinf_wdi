de7cf1fc885c9706700446c30918858dbe53d642
Krzysztof Nagrodkiewicz <kn334812@mimuw.edu.pl>
2015-12-20 07:21:55 -0800

    Dodane rozwiazanie kolokwium poprawkowego 2014.

--- kolokwia/2/2014p.py
+++ kolokwia/2/2014p.py
@@ -8,22 +8,22 @@ __author__ = 'kris'
 
 
 def is_loop2(filename):
-	moves = {}
-	with open(filename, 'r') as f:
-		for line in f:
-			row = map(int, line.split())
-			first = (row[0], row[1])
-			second = (row[2], row[3])
-			if first not in moves:
-				moves[first] = [second]
-			else:
-				moves[first].append(second)
+    moves = {}
+    with open(filename, 'r') as f:
+        for line in f:
+            row = map(int, line.split())
+            first = (row[0], row[1])
+            second = (row[2], row[3])
+            if first not in moves:
+                moves[first] = [second]
+            else:
+                moves[first].append(second)
 
-	for move in moves:
-		for m in moves[move]:
-			if move in moves[m]:
-				return True
-	return False
+    for move in moves:
+        for m in moves[move]:
+            if move in moves[m]:
+                return True
+    return False
 
 
 # Zadanie 2
@@ -59,21 +59,22 @@ Niemniej listy lu nie zmieniamy w żaden sposób ponieważ wewnątrz funkcji lie
 zupełnie odrębna lista, czyli w efekcie wypisze się napis [1, 2, 3, -3, -2, -1, -3, -2, -3] [-3, -2, -1].
 '''
 
+
 # Zadanie 3
 
 # Prawdopodobnie da się to zrobić mądrzej XD
 
 def sekwencje(plik):
-	reg = r'[A]{5,}[GCT]|[T]{5,}[GCA]|[G]{5,}[ATC]|[C]{5,}[ATG]'
-	with open(plik, 'r') as f:
-		for line in f:
-			matches = re.findall(reg, line)
-			for i in range(len(matches)):
-				for k in range(i+1, len(matches)):
-					if (matches[k][0] == 'T' and matches[i][0] == 'A') \
-							or (matches[k][0] == 'A' and matches[i][0] == 'T') \
-							or (matches[k][0] == 'G' and matches[i][0] == 'C') \
-							or (matches[k][0] == 'C' and matches[i][0] == 'G'):
-						if len(matches[k]) > len(matches[i])*2-2:
-							print line, #na koncu linii jest zbedny \n
-							break
+    reg = r'[A]{5,}[GCT]|[T]{5,}[GCA]|[G]{5,}[ATC]|[C]{5,}[ATG]'
+    with open(plik, 'r') as f:
+        for line in f:
+            matches = re.findall(reg, line)
+            for i in range(len(matches)):
+                for k in range(i + 1, len(matches)):
+                    if (matches[k][0] == 'T' and matches[i][0] == 'A') \
+                        or (matches[k][0] == 'A' and matches[i][0] == 'T') \
+                        or (matches[k][0] == 'G' and matches[i][0] == 'C') \
+                        or (matches[k][0] == 'C' and matches[i][0] == 'G'):
+                        if len(matches[k]) > len(matches[i]) * 2 - 2:
+                            print line,  # na koncu linii jest zbedny \n
+                            break
